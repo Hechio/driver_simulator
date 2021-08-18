d0f35cd636ed2248159d7f283c5c0d2474706c93
Steve Hechio <stevehechio@Steves-iMac.local>
2021-08-18 21:21:17 +0300

    plane programming game

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float rotationSpeed;
+    public float verticalInput;
+    private GameObject propeller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        propeller = GameObject.Find("Propeller");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        propeller.transform.Rotate(Vector3.forward, 3000.0f * Time.deltaTime);
+    }
+}
